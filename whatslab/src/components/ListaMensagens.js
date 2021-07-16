187b32dadfb61863a79057ff3da852d4609cbc51
joaofilippe <joaofilippe@outlook.com>
2021-07-16 16:10:43 -0300

    feat/ adicionando a base

---
+++ whatslab/src/components/ListaMensagens.js
@@ -0,0 +1,10 @@
+import react from 'React';
+import styled from 'styled-components';
+
+class ListaMensagens extends React.Component {
+    render () {
+        return (
+        
+        )
+    }
+}
